7cd8b65b4ee31e26bb1633f45cd2faffa4f89c04
Kurt Johnson <kujotx@gmail.com>
2013-04-08 10:57:49 -0500

    update to 1.0.3.9-beta

--- clipx/tools/chocolateyInstall.ps1
+++ clipx/tools/chocolateyInstall.ps1
@@ -1,2 +1,2 @@
-Install-ChocolateyPackage 'clipx' 'EXE' '/S' 'http://bluemars.org/clipx/clipx-1.0.3.8-setup.exe'
+Install-ChocolateyPackage 'clipx' 'EXE' '/S' 'http://bluemars.org/clipx/clipx-1.0.3.9g-setup.exe' 'http://bluemars.org/clipx/clipx-1.0.3.9g-setup-x64.exe'
  
