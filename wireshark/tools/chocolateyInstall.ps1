d09ad057e5dec67fe14d8ff7e81d226bf8d2a7e8
Kurt Johnson <kujotx@gmail.com>
2013-12-23 10:27:01 -0600

    they changed up the download URL

--- wireshark/tools/chocolateyInstall.ps1
+++ wireshark/tools/chocolateyInstall.ps1
@@ -1,7 +1,7 @@
 ﻿$packageName = "wireshark"
 $fileType = "exe"
 $silentArgs = "/S /quicklaunchicon=no"
-$url = "http://wiresharkdownloads.riverbed.com/wireshark/win32/Wireshark-win32-1.10.0.exe"
-$url64bit = "http://wiresharkdownloads.riverbed.com/wireshark/win64/Wireshark-win64-1.10.0.exe"
+$url = "http://wiresharkdownloads.riverbed.com/wireshark/win32/Wireshark-win32-1.10.2.exe"
+$url64bit = "http://wiresharkdownloads.riverbed.com/wireshark/win64/Wireshark-win64-1.10.2.exe"
 
 Install-ChocolateyPackage $packageName $fileType $silentArgs $url $url64bit
