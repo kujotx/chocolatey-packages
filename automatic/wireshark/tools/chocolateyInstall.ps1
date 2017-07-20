2fbbef9f0453fea271c0cb99e1e22b71213ef7e1
Chocolatey <chocolatey@realdimensions.net>
2017-07-20 01:05:08 +0000

    AU: 1 updated - wireshark [skip ci]

--- automatic/wireshark/tools/chocolateyInstall.ps1
+++ automatic/wireshark/tools/chocolateyInstall.ps1
@@ -1,10 +1,10 @@
 ﻿$ErrorActionPreference = 'Stop'
 
 $packageName = 'WireShark'
-$url32       = 'https://1.na.dl.wireshark.org/win32/Wireshark-win32-2.2.8.exe'
-$checksum32  = '7bfd50b9bbeeba6cc55c8f660e9e44c643791ee306227584299b560843f1564c'
-$url64	     = 'https://1.na.dl.wireshark.org/win64/Wireshark-win64-2.2.8.exe'
-$checksum64  = 'caa8e6b1a69964594cfc5d4ebd16255a8ba80f54044c0d9c9352a798bde2bc89'
+$url32       = 'https://1.na.dl.wireshark.org/win32/Wireshark-win32-2.4.0.exe'
+$checksum32  = '971e6021d6ba214aa938ff43436b70525efc8f5286e556066dfa4863603e7c5e'
+$url64	     = 'https://1.na.dl.wireshark.org/win64/Wireshark-win64-2.4.0.exe'
+$checksum64  = '341f97b1d8fc25b4e149fb20b718451aa8646856faa0576071037e60ed47f8b0'
 
 $toolsPath   = Split-Path $MyInvocation.MyCommand.Definition
 
