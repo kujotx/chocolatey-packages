7c1184953ecb605891a933d43771dacd13769811
Chris Charabaruk <chris.charabaruk@outlook.com>
2019-01-01 11:31:32 -0500

    Replace aliases with proper command names
    Avoid potential problems with alias redefinitions.

--- automatic/winmerge/update.ps1
+++ automatic/winmerge/update.ps1
@@ -17,13 +17,13 @@ function global:au_GetLatest {
     $download_page = Invoke-WebRequest -Uri $releases
 
     $re32  = "winmerge-.*-Setup.exe"
-    $url32 = $download_page.links | ? href -match $re32 | select -First 1 -expand href
+    $url32 = $download_page.links | where-object href -match $re32 | select-object -First 1 -expand href
     $re64  = "winmerge-.*-x64-Setup.exe"
-    $url64 = $download_page.links | ? href -match $re64 | select -First 1 -expand href
+    $url64 = $download_page.links | where-object href -match $re64 | select-object -First 1 -expand href
 
-    $version = $url32 -split '-|.exe' | select -Last 1 -Skip 2
+    $version = $url32 -split '-|.exe' | select-object -Last 1 -Skip 2
 
     return @{ URL32 = $url32; URL64 = $url64; Version = $version }
 }
 
-update -ChecksumFor all
+update-package -ChecksumFor all
