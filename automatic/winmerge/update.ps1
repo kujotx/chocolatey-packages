a6dc010667dbbe328d86d008c9c00d8a8ae2c43c
Chris Charabaruk <chris.charabaruk@outlook.com>
2019-01-01 11:28:53 -0500

    Add 64-bit installer support for WinMerge

--- automatic/winmerge/update.ps1
+++ automatic/winmerge/update.ps1
@@ -7,6 +7,8 @@ function global:au_SearchReplace {
         ".\tools\chocolateyInstall.ps1" = @{
             "(^[$]url32\s*=\s*)('.*')"      = "`$1'$($Latest.URL32)'"
             "(^[$]checksum32\s*=\s*)('.*')" = "`$1'$($Latest.Checksum32)'"
+            "(^[$]url64\s*=\s*)('.*')"      = "`$1'$($Latest.URL64)'"
+            "(^[$]checksum64\s*=\s*)('.*')" = "`$1'$($Latest.Checksum64)'"
         }
     }
 }
@@ -14,12 +16,14 @@ function global:au_SearchReplace {
 function global:au_GetLatest {
     $download_page = Invoke-WebRequest -Uri $releases
 
-    $re  = "winmerge-.*-Setup.exe"
-    $url = $download_page.links | ? href -match $re | select -First 1 -expand href
-    
-    $version = $url -split '-|.exe' | select -Last 1 -Skip 2
+    $re32  = "winmerge-.*-Setup.exe"
+    $url32 = $download_page.links | ? href -match $re32 | select -First 1 -expand href
+    $re64  = "winmerge-.*-x64-Setup.exe"
+    $url64 = $download_page.links | ? href -match $re64 | select -First 1 -expand href
 
-    return @{ URL32 = $url; Version = $version }
+    $version = $url32 -split '-|.exe' | select -Last 1 -Skip 2
+
+    return @{ URL32 = $url32; URL64 = $url64; Version = $version }
 }
 
-update -ChecksumFor 32
+update -ChecksumFor all
